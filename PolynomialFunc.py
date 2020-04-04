046be05e1bda8994d33afe09d9f84c0f6516b922
Yigit Tasoglu <yctasoglu@gmail.com>
2020-04-04 21:29:46 +0300

    Polynomial Feature Extraction

---
+++ PolynomialFunc.py
@@ -0,0 +1,27 @@
+#HighDimensionalSpace
+#PCA Tryout
+#https://www.kaggle.com/pmarcelino/data-analysis-and-feature-extraction-with-python
+
+from matplotlib import pyplot as plt
+from matplotlib.colors import ListedColormap
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
+from sklearn.preprocessing import PolynomialFeatures
+
+import os
+cwd = os.path.dirname(os.getcwd()) 
+print(cwd)
+#Get Data Frame from local parquet file
+df = pd.read_parquet(r'path')
+
+#Slice target feature
+y = df["target"]
+#Slice input feature and split target from input features.
+X = df.drop(columns=["target"])
+
+poly = PolynomialFeatures(degree=6).fit(X)
+X = poly.transform(X)
+print("Get Polynomial Features: ",poly.get_feature_names())
